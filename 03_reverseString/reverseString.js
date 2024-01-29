bc761e96033259b809ff3eb0af18c8bd008001d0
Sunil Patil <sunilpatilsnd@gmail.com>
2024-01-29 15:10:34 +0530

    solve exrecise 01 to 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('');
+    let position = str.length -1 ;
+    let result = '';
+    for( i = position ; i >= 0 ; i--){
+        result += strArr[i]; 
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
 const removeFromArray = function() {
-
+    const inputArr = arguments[0]; // arguments is default array of arguments that are passed to a function
+    let result = inputArr;
+    for(let i = 1 ; i < arguments.length ; i++){
+        result = removeOneItemFromArray(result,arguments[i])
+    }
+    return result;
 };
+
+function removeOneItemFromArray(inputArray, itemToRemove){
+    let result = [];
+    let arraySize = inputArray.length;
+    for(let i = 0 ; i < arraySize; i++){
+        if( inputArray[i] !== itemToRemove){
+            result.push(inputArray[i]);
+        }
+    }
+    return result;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
